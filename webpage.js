3720541d6d3efac4ccb271a5d7f1c8e9e589a88f
Morgan <52506620+varMorgan@users.noreply.github.com>
2019-07-03 13:12:55 -0500

    Add files via repository

---
+++ webpage.js
@@ -0,0 +1,10 @@
+
+// window.alert("Have Fun on my website HEHEHEHE");
+
+function smileyFace() {
+  document.getElementById("Name").innerHTML = "Morgan Glover :) HeHe";
+}
+
+function AboutMe() {
+  document.getElementById().innerHTML = "";
+}
